e8a65de41a217a875a0087d5f216ff4b8b1782d2
AneezaImtiaz <aneezaimtiaz777@gmail.com>
2024-08-12 20:42:02 +0200

    Add header

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="header">
+    <h1 class="title">{{ APP_TITLE }}</h1>
+    <div class="navbar">
+      <ul class="navbar-list">
+        <li :class="['navbar-item', isSelected('/') ? 'selected' : '']">
+          <a href="/">{{ ROUTES.home }}</a>
+        </li>
+        <!-- All other navigation items you want to add in header -->
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import './Header.css';
+import { APP_TITLE, ROUTES } from '@//utils/Constants';
+
+export default defineComponent({
+  name: 'Header',
+  data() {
+    return {
+      APP_TITLE,
+      ROUTES,
+    };
+  },
+  methods: {
+    isSelected(path: string): boolean {
+      return window.location.pathname === path;
+    },
+  },
+});
+</script>
---
+++ src/utils/Constants.ts
@@ -0,0 +1,5 @@
+export const APP_TITLE = 'Task Master' as const;
+
+export const ROUTES = {
+  home: 'Home',
+} as const;
